68bd52c9f0f2ec659f9a6f32b1bd6a7d6bbcdb66
nyk.yeon <nyk.yeon@gmail.com>
2023-03-07 13:08:08 +0900

    예제 추가

--- examples/MH-Z14A/MH-Z14A.ino
+++ examples/MH-Z14A/MH-Z14A.ino
@@ -1,20 +1,26 @@
-#include <SoftwareSerial.h>
+// #include <SoftwareSerial.h>
 #include <MHZ.h>
 
 // pin for pwm reading
 #define CO2_IN 10
 
-// pin for uart reading
-#define MH_Z19_RX D4  // D7
-#define MH_Z19_TX D0  // D6
+// pin for uart reading (SoftwareSerial)
+#define MH_Z14_RX D4  // D7
+#define MH_Z14_TX D0  // D6
 
-MHZ co2(MH_Z19_RX, MH_Z19_TX, CO2_IN, MHZ19B);
+//MHZ co2(MH_Z19_RX, MH_Z19_TX, CO2_IN, MHZ14A);
+//MHZ co2(Serial2, CO2_IN, MHZ14A);
+MHZ co2(Serial2, MHZ14A);
 
 void setup() {
   Serial.begin(9600);
-  pinMode(CO2_IN, INPUT);
+  
+  // use HW serial
+  Serial2.begin(9600);  // ESP32 uart2 - Pin Rx: 16 / Tx: 17  
+
+  // pinMode(CO2_IN, INPUT); // use pwm
   delay(100);
-  Serial.println("MHZ 19B");
+  Serial.println("MHZ 14A");
 
   // enable debug to get addition information
   // co2.setDebug(true);
@@ -43,18 +49,11 @@ void loop() {
     Serial.print("n/a");
   }
 
+  /* use pwm
   int ppm_pwm = co2.readCO2PWM();
   Serial.print(", PPMpwm: ");
   Serial.print(ppm_pwm);
-
-  int temperature = co2.getLastTemperature();
-  Serial.print(", Temperature: ");
-
-  if (temperature > 0) {
-    Serial.println(temperature);
-  } else {
-    Serial.println("n/a");
-  }
+  */
 
   Serial.println("\n------------------------------");
   delay(5000);
