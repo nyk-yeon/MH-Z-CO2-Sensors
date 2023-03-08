fba1f223aa0f5f7edfdf7f70ddd30aab0aa410c3
nyk.yeon <nyk.yeon@gmail.com>
2023-03-08 10:20:32 +0900

    예제 추가/분리

--- examples/MH-Z14A_SWserial/MH-Z14A_SWserial.ino
+++ examples/MH-Z14A_SWserial/MH-Z14A_SWserial.ino
@@ -1,18 +1,26 @@
+#include <SoftwareSerial.h>
+//EspSoftwareSerial:  https://github.com/plerup/espsoftwareserial
 #include <MHZ.h>
 
 // pin for pwm reading
 #define CO2_IN 18
 
-// MHZ co2(Serial2, MHZ14A);                  // Serial only
-// MHZ co2(Serial2, CO2_IN, MHZ14A);
-MHZ co2(Serial2, CO2_IN, MHZ14A, RANGE_10K);
+// pin for uart reading (SoftwareSerial)
+#define RX_Pin 22 // Connect sensor Tx
+#define TX_Pin 23 // Connect sensor Rx
+
+SoftwareSerial SWSerial;
+
+//MHZ co2(SWSerial, MHZ14A);              // Serial only
+//MHZ co2(SWSerial, CO2_IN, MHZ14A);
+MHZ co2(SWSerial, CO2_IN, MHZ14A, RANGE_5K);
 
 
 void setup() {
   Serial.begin(115200);
   
-  // Use HW serial - UART2
-  Serial2.begin(9600);  // ESP32 uart2 - Pin Rx: 16 / Tx: 17  
+  // use SW serial
+  SWSerial.begin(9600, SWSERIAL_8N1, RX_Pin, TX_Pin);
 
   // pwm pin set
   pinMode(CO2_IN, INPUT); 
@@ -25,7 +33,7 @@ void setup() {
 
   // Enable debug to get addition information
   // co2.setDebug(true);
- 
+
   // Sensor Preheat time - MHZ14A 3min
   if (co2.isPreHeating()) {
     Serial.print("Preheating");
